1d14bc9a493e4830fa385234f3cb957c3efcf566
gustavodemari <guhdemari@gmail.com>
2014-05-16 00:42:12 -0300

    updating product object to a list with objects

--- app.js
+++ app.js
@@ -3,15 +3,24 @@
   var app = angular.module('store', []);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem = {
-    name: 'Dodecahedron',
-    price: 2.95,
-    description: '. . .',
-    canPurchase: false,
-    soldOut: true
-  };
+  var gems = [
+    {
+      name: 'Dodecahedron',
+      price: 2.95,
+      description: '. . .',
+      canPurchase: false,
+      soldOut: true
+    },
+    {
+      name: 'Pentagonal Gem',
+      price: 5.95,
+      description: '. . .',
+      canPurchase: true,
+      soldOut: false
+    },
+  ];
 }
 )();
